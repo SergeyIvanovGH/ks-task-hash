a49c820e4f77645c8a49d698b1ee4829560bb5da
Serhii Ivanov <sergey.ivanov@meta.ua>
2019-03-28 18:37:33 +0200

    first commit

---
+++ src/main/java/com/sivanov/kstaskhash/KsTaskHashApplication.java
@@ -0,0 +1,13 @@
+package com.sivanov.kstaskhash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsTaskHashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsTaskHashApplication.class, args);
+	}
+
+}
